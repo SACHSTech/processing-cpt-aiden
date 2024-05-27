59bb5b19c27368d3d0ad568f4708a5293d8ecee2
AidenRazack <aiden.razack25@ycdsbk12.ca>
2024-05-27 15:36:14 -0400

    started

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,102 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
-public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
-  public void settings() {
-	// put your size call here
-    size(400, 400);
+  public class Sketch extends PApplet {
+    
+    
+    /**
+     * Called once at the beginning of execution, put your size all in this method
+     */
+
+    // Define game states and variables for cutscenes
+    final int OPENING = 0;
+    final int MENU = 1;
+    final int CUTSCENE2 = 2;
+    final int GAME = 3;
+    
+    int state = OPENING;
+
+    PImage opening;
+    PImage cutscene1;
+    
+    // Exstablish variables for the cutscene time
+    int cutsceneStartTime;
+    int cutsceneDuration = 5000; 
+
+    public static void main(String[] args) {
+        String[] processingArgs = {"MySketch"};
+        Sketch mySketch = new Sketch();
+        PApplet.runSketch(processingArgs, mySketch);
+    }
+
+    @Override
+    public void settings() {
+        size(800, 600);
+    }
+
+    @Override
+    public void setup() {
+        opening = loadImage("cutscene1.gif");
+        cutscene1 = loadImage("cutscene2.gif");
+        
+        cutsceneStartTime = millis();
+    }
+
+    @Override
+    public void draw() {
+        switch (state) {
+            case OPENING:
+                drawCutscene1();
+                if (millis() - cutsceneStartTime > cutsceneDuration) {
+                    state = MENU;
+                }
+                break;
+            case MENU:
+                drawMenu();
+                break;
+            case CUTSCENE2:
+                drawCutscene2();
+                if (millis() - cutsceneStartTime > cutsceneDuration) {
+                    state = GAME;
+                }
+                break;
+            case GAME:
+                drawGame();
+                break;
+        }
+    }
+
+    void drawCutscene1() {
+        background(0);
+        image(cutscene1, 0, 0, width, height);
+    }
+
+    void drawMenu() {
+        background(100);
+        fill(255);
+        textSize(32);
+        textAlign(CENTER, CENTER);
+        text("Press 'Enter' to Start Next Cutscene", width / 2, height / 2);
+    }
+
+    void drawCutscene2() {
+        background(0);
+        image(cutscene1, 0, 0, width, height);
+    }
+
+    void drawGame() {
+        background(50);
+        fill(255);
+        textSize(32);
+        textAlign(CENTER, CENTER);
+        text("Game Screen", width / 2, height / 2);
+    }
+
+    @Override
+    public void keyPressed() {
+        if (state == MENU && keyCode == ENTER) {
+            state = CUTSCENE2;
+            cutsceneStartTime = millis();
+        }
+    }
   }
-
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
-  public void setup() {
-    background(210, 255, 173);
-  }
-
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
-  
-  // define other methods down here.
-}
